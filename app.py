2c63d9b63c9b95ef0dd8c3bed7152fba2c369f43
Eric Gitonga <gitonga@gmail.com>
2022-03-15 01:09:53 +0300

    move food info under food pic bit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit_analytics
 
-st.title("Food")
+st.title("Expectations vs. Reality")
 
 with streamlit_analytics.track():
     food_on_menu = st.sidebar.file_uploader("Upload a photo of the food as shown on the menu.")
@@ -15,15 +15,15 @@ with streamlit_analytics.track():
             st.markdown("# Food Received")
             st.image(food_received)
         
-    app = st.selectbox("What app did you order from?",
-                      ["Glovo", "Jumia", "Uber Eats", "Other"])
-    if app == "Other":
-        app = st.text_input("If other, what app did you use?")
-    eatery = st.selectbox("What restaurant did you order from?",
-                         ["Java", "Art Caffe", "Urban Burger",
-                          "Pizza Inn", "Other"])
-    if eatery == "Other":
-        eatery = st.text_input("If other, what restaurant did you order from?")
-    period = st.date_input("When did you order it?")
-    if st.button("Click to submit information"):
-        st.text("I ordered the above from {} using the {} app on {}".format(eatery, app, period))
+        app = st.selectbox("What app did you order from?",
+                          ["Glovo", "Jumia", "Uber Eats", "Other"])
+        if app == "Other":
+            app = st.text_input("If other, what app did you use?")
+        eatery = st.selectbox("What restaurant did you order from?",
+                             ["Java", "Art Caffe", "Urban Burger",
+                              "Pizza Inn", "Other"])
+        if eatery == "Other":
+            eatery = st.text_input("If other, what restaurant did you order from?")
+        period = st.date_input("When did you order it?")
+        if st.button("Click to submit information"):
+            st.text("I ordered the above from {} using the {} app on {}".format(eatery, app, period))
