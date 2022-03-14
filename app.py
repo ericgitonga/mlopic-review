f67f758e4bbae85b1b3bfc3c7766ec183cfd17dc
Eric Gitonga <gitonga@gmail.com>
2022-03-14 23:07:29 +0300

    changed to file uploader

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.title("Food")
 
-if st.sidebar.checkbox("Select to activate camera"):
-    food = st.sidebar.camera_input("take a pic")
-    if food is not None:
-        st.image(food)
+food = st.sidebar.file_uploader("Upload a photo of food")
+if food is not None:
+    st.image(food)
