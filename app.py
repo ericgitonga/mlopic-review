7e187f8dcaaccd9bed1fc0903d86a6b9320b5b18
Eric Gitonga <gitonga@gmail.com>
2022-03-14 23:59:32 +0300

    added options to select app and eatery

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ with streamlit_analytics.track():
     if food is not None:
         st.image(food)
         
-        with st.form("Food feedback"):
-            app = st.text_input("What app did you order from?")
-            period = st.date_input("When did you order it?")
-            st.form_submit_button("Click to submit")
-        
-        st.text("Ordered from {} on {}".format(app, period))
+#        with st.form("Food feedback"):
+        app = st.selectbox("What app did you order from?",
+                          ["Glovo", "Jumia", "Uber Eats", "Other"])
+        if app == "Other":
+            app = st.text_input("If other, what app did you use?")
+        eatery = st.selectbox("What restaurant did you order from?",
+                             ["Java", "Art Caffe", "Urban Burger",
+                              "Pizza Inn", "Other"])
+        if eatery == "Other":
+            eatery = st.text_input("If other, what restaurant did you order from?")
+        period = st.date_input("When did you order it?")
+#            st.form_submit_button("Click to submit")
+        if st.button("Click to submit information"):
+            st.text("I ordered the above from {} using the {} app on {}".format(eatery, app, period))
