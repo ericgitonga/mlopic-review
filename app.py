02d5d0e755b07cf1ca8e5331398205553000dcea
Eric Gitonga <gitonga@gmail.com>
2022-03-14 23:47:46 +0300

    add form to get details

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ with streamlit_analytics.track():
     food = st.sidebar.file_uploader("Upload a photo of food")
     if food is not None:
         st.image(food)
+        
+        with st.form("Food feedback"):
+            app = st.text_input("What app did you order from?")
+            period = st.date_input("When did you order it?")
+            st.form_submit_button("Click to submit")
+        
+        st.text("Ordered from {} on {}".format(app, period))
