b250b95599d35e068e1ddcb26c6e244257391f78
Eric Gitonga <gitonga@gmail.com>
2022-03-14 23:02:27 +0300

    added camera input

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Food")
+
+if st.sidebar.checkbox("Select to activate camera"):
+    food = st.sidebar.camera_input("take a pic")
+    if food is not None:
+        st.image(food)
